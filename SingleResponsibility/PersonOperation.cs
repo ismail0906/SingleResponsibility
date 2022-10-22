70e9d9b51b2371e5a34f0f9c2b1acccb13541074
ASUS <ASUS@ISMAIL>
2022-10-22 15:05:13 +0300

    Add project files.

---
+++ SingleResponsibility/PersonOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class PersonOperation
+    {
+        public Person[] peoples = new Person[0];
+
+        public void Add(Person people) 
+        {
+            Array.Resize(ref peoples,peoples.Length+1);
+            peoples[peoples.Length-1] = people;
+        }
+        public void ShowPersons() 
+        {
+            foreach (Person person in peoples)
+            {
+                Console.WriteLine($"{person.Name} - {person.Surname}");
+            }
+
+        }
+
+    }
+}
---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,17 @@
+﻿using SingleResponsibility;
+
+Person p = new Person();
+p.Name = "Ekrem";
+p.Surname = "Yıldırım";
+
+Person p2 =new Person();
+p2.Name = "Onur";
+p2.Surname = "Derman";
+
+PersonOperation operation = new PersonOperation();
+operation.Add(p);
+operation.Add(p2);
+
+operation.ShowPersons();
+
+Console.ReadKey();
